99b743e563d937b15f0e8dd7c3b7465c6b213deb
Maddie A <98988491+maddieadair@users.noreply.github.com>
2024-03-11 16:02:14 -0500

    Add Footer Component

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Logo from "../assets/unnamed.png";
+
+export default function Footer() {
+  return (
+    <div className="flex place-content-center pb-24">
+      <div className="flex flex-row space-x-10 w-2/3 justify-center items-center">
+        <div class="flex justify-center h-32">
+          <img
+            class="object-scale-down max-h-full w-fit"
+            src={Logo}
+            alt=""
+          />
+        </div>
+        <div className="flex flex-col">
+          <h3 className="font-bold uppercase">
+            The Museum of Fine Arts, Houston
+          </h3>
+          <h4>Main Address: 1001 Bissonet, Houston, Texas 77005</h4>
+          <h4>Main Information Line: 713.639.7300</h4>
+          <h4>Copyright © 2024, The Museum of Fine Arts, Houston.</h4>
+          <h4>All rights reserved.</h4>
+        </div>
+      </div>
+    </div>
+  );
+}
