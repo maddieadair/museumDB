5a5b79772444d4dee44e9a3e7c75b95f397fb55a
Maddie A <98988491+maddieadair@users.noreply.github.com>
2024-03-11 15:57:41 -0500

    Add Login page

---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,12 @@
+import { React, useState } from 'react';
+
+export default function Login(){
+    const [username, setUsername] = useState();
+    const [password, setPassword] = useState();
+    
+    return (
+        <div>
+            Login
+        </div>
+    )
+}
