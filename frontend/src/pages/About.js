2d268a3fa72bb657f7bb62849b143111ae92fced
Maddie A <98988491+maddieadair@users.noreply.github.com>
2024-03-11 15:58:20 -0500

    Add About page

---
+++ frontend/src/pages/About.js
@@ -0,0 +1,13 @@
+//import { useState } from 'react';
+//import { useNavigate } from 'react-router-dom';
+import React from "react";
+
+export default function About () {
+
+    return (
+        <div>About
+
+        </div>
+    )
+}
+
