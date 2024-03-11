079807fef27bb4ee1f8e3b85a6e58620efd6b665
Maddie A <98988491+maddieadair@users.noreply.github.com>
2024-03-11 16:10:33 -0500

    Add Route paths

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+// import { Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Exhibitions from "./pages/Exhibitions";
+import Collections from "./pages/Collections";
+import Shop from "./pages/Shop";
+import Login from "./pages/Login";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-ivory min-h-screen">
+      <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/About" element={<About />}></Route>
+            <Route path="/Exhibitions" element={<Exhibitions />}></Route>
+            <Route path="/Collections" element={<Collections />}></Route>
+            <Route path="/Shop" element={<Shop />}></Route>
+            <Route path="/Login" element={<Login />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
