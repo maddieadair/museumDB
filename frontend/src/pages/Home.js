e1f5e5686bf113873edcab621ee52cc5678fbf67
Maddie A <98988491+maddieadair@users.noreply.github.com>
2024-03-21 18:07:40 -0500

    Update Home page

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,35 +1,56 @@
-//import { useState } from 'react';
-//import { useNavigate } from 'react-router-dom';
-import React from "react";
-import { Link } from "react-router-dom";
-import Giordano from "../assets/Luca_Giordano_-_Allegory_of_Prudence_-_Google_Art_Project.jpg";
-import MFA from "../assets/MFAH.Beck-View-S-Interior.jpg";
-import Ferri from "../assets/Ciro_Ferri_-_Moses_and_the_Daughters_of_Jethro_-_BF.1982.5_-_Museum_of_Fine_Arts.jpg";
-import Pietro from "../assets/Pietro_da_Cortona_-_Saint_Constantia's_Vision_before_the_Tomb_of_Saints_Agnes_and_Emerentiana_-_Google_Art_Project.jpg";
-import Gallery from "../assets/visitors-in-beck-gallery.4566162743393514636.jpg";
-
-import WelcomeNavbar from "../components/WelcomeNavbar";
+import { React, useState } from "react";
+import UserNavbar from "../components/UserNavbar";
+import Angelico from "../assets/images/1200px-Fra_Angelico_-_Saint_Anthony_Abbot_Shunning_the_Mass_of_Gold_-_Google_Art_Project.jpg";
+import { IoArrowForwardCircleOutline } from "react-icons/io5";
+import HomeExhibitions from "../components/HomeExhibitions";
+import HomeStore from "../components/HomeStore";
 import Footer from "../components/Footer";
-import HomepageCollections from "../components/HomepageCollections";
-import HomepageExhibitions from "../components/HomepageExhibitions";
 
-export default function About() {
+export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen space-y-24 font-hk-grotesk">
-      <div>
-          <WelcomeNavbar />
-          <h1 className="absolute z-10 text-ivory text-7xl font-black bottom-20 left-20 font-goudy-sm">
+    <div className="min-h-screen">
+      <UserNavbar />
+      <div className="h-screen flex flex-row pt-36 pb-12 px-16 gap-x-8 border-b">
+        <div className="flex flex-col w-1/2 gap-y-8 p-6">
+          <h1 className="font-fanwoodText italic text-8xl">
             The Museum of Fine Arts, Houston
           </h1>
-          <div className="h-min overflow-hidden rounded-md">
-            <img
-              className="object-cover max-h-full w-full m-auto hover:scale-125 transition-all duration-700 ease-in-out"
-              src={Pietro}
-              alt=""
-            />
-          </div>
+          <p className="font-bold uppercase text-cinnabar">Since 1990</p>
+          <p>
+            The Museum of Fine Arts, Houston, serves as a welcoming and
+            inclusive place for all people, connecting the communities of
+            Houston with diverse histories of art spanning 5,000 years and six
+            continents.
+          </p>
+          <p>
+            Through our permanent collections, special exhibitions, learning and
+            interpretation programs, studio instruction, publications,
+            conservation, and scholarly research, we strive to inspire
+            appreciation and understanding of the broadest spectrum of human
+            achievement.
+          </p>
+        </div>
+        <div className="w-2/3 flex flex-row space-x-2 outline outline-offset-8 outline-gray-400 px-2 justify-center rounded-xl">
+          <img
+            className="brightness-75 rounded-xl object-cover object-left w-1/3 "
+            src={Angelico}
+            alt=""
+          />
+          <img
+            className="brightness-75 rounded-xl object-cover object-center w-1/3"
+            src={Angelico}
+            alt=""
+          />
+          <img
+            className="brightness-75 rounded-xl object-cover object-right w-1/3"
+            src={Angelico}
+            alt=""
+          />
+        </div>
       </div>
-      <HomepageExhibitions />
+
+      <HomeExhibitions />
+      <HomeStore />
       <Footer />
     </div>
   );
