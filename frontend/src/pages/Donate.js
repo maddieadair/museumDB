5eb65537aab13032d970f31c4020adeaa76a87fb
Maddie A <98988491+maddieadair@users.noreply.github.com>
2024-03-22 16:38:05 -0500

    Remove unnecessary donor information fields

--- frontend/src/pages/Donate.js
+++ frontend/src/pages/Donate.js
@@ -7,24 +7,8 @@ import Footer from "../components/Footer";
 export default function Donate() {
   const donationAmount = [5, 10, 25, 50, 100, 250, 500];
   const [amount, setAmount] = useState(5);
+  const [otherOpen, setOtherOpen] = useState(false);
 
-  const [donationNote, setDonationNote] = useState("");
-  const [donorFname, setDonorFname] = useState("");
-  const [donorMname, setDonorMname] = useState("");
-  const [donorLname, setDonorLname] = useState("");
-  const [donorEmail, setDonorEmail] = useState("");
-  const [donorAddress, setDonorAddress] = useState("");
-  const [donorCity, setDonorCity] = useState("");
-  const [donorState, setDonorState] = useState("");
-  const [donorZip, setDonorZip] = useState("");
-  const [donorPhone, setDonorPhone] = useState("");
-
-  const [cardNumber, setCardNumber] = useState("");
-  const [cardExp, setCardExp] = useState("");
-  const [cardFname, setCardFname] = useState("");
-  const [cardLname, setCardLname] = useState("");
-  const [cardExpDate, setCardExpDate] = useState("");
-  const [cardCVC, setCardCVC] = useState("");
   return (
     <div className="min-h-screen">
       <UserNavbar />
@@ -60,187 +44,70 @@ export default function Donate() {
             />
           </div>
 
-
           <form className="flex flex-col space-y-12">
-        <div className="flex flex-col space-y-20">
-          <div className="flex flex-col space-y-10 w-fit">
-            <h2 className="font-fanwoodText text-5xl">Donation</h2>
-            <div className="flex flex-row space-x-4">
-              {donationAmount.map((number, index) => (
-                <p
-                  key={index}
-                  className="rounded-full border border-obsidian text-obsidian w-fit py-2 px-6 hover:bg-obsidian hover:text-chalk"
-                  onClick={() => setAmount(number)}
-                >
-                  ${number}
-                </p>
-              ))}
-            </div>
-
-            <div className="flex flex-col space-y-2 w-1/2">
-              <label className="font-bold">Amount</label>
-              <input
-                type="text"
-                value={amount}
-                onChange={(e) => setAmount(e.target.value)}
-                className="bg-white border border-obsidian rounded-md p-2"
-              ></input>
-            </div>
-            <div className="flex flex-col space-y-2">
-              <label className="font-bold">Donation Note</label>
-              <textarea
-                className="bg-white border border-obsidian rounded-md p-2"
-                rows="4"
-              ></textarea>
-            </div>
-          </div>
-
-          <div className="flex flex-col space-y-8">
-            <h3 className="text-5xl font-fanwoodText">Contact Information</h3>
-            <div className="flex flex-col space-y-6">
-              <div className="flex flex-col space-y-2">
-                <label className="font-bold">First Name</label>
-                <input
-                  type="text"
-                  className="bg-white border border-obsidian rounded-md p-2"
-                ></input>
-              </div>
-
-              <div className="flex flex-col space-y-2">
-                <label className="font-bold">Middle Name</label>
-                <input
-                  type="text"
-                  className="bg-white border border-obsidian rounded-md p-2"
-                ></input>
-              </div>
-
-              <div className="flex flex-col space-y-">
-                <label className="font-bold">Last Name</label>
-                <input
-                  type="text"
-                  className="bg-white border border-obsidian rounded-md p-2"
-                ></input>
-              </div>
-
-              <div className="flex flex-col space-y-">
-                <label className="font-bold">Email Address</label>
-                <input
-                  type="text"
-                  className="bg-white border border-obsidian rounded-md p-2"
-                ></input>
-              </div>
-
-              <div className="flex flex-col space-y-">
-                <label className="font-bold">Phone Number</label>
-                <input
-                  type="text"
-                  className="bg-white border border-obsidian rounded-md p-2"
-                ></input>
-              </div>
-            </div>
-          </div>
-
-          <div className="flex flex-col space-y-8">
-            <h3 className="text-5xl font-fanwoodText">Billing Information</h3>
-            <div className="flex flex-col space-y-6">
-              <div className="flex flex-col space-y-2">
-                <label className="font-bold">Street Address</label>
-                <input
-                  type="text"
-                  className="bg-white border border-obsidian rounded-md p-2"
-                ></input>
-              </div>
-
-              <div className="flex flex-col space-y-2">
-                <label className="font-bold">City</label>
-                <input
-                  type="text"
-                  className="bg-white border border-obsidian rounded-md p-2"
-                ></input>
-              </div>
-
-              <div className="flex flex-col space-y-">
-                <label className="font-bold">State</label>
-                <input
-                  type="text"
-                  className="bg-white border border-obsidian rounded-md p-2"
-                ></input>
-              </div>
-
-              <div className="flex flex-col space-y-">
-                <label className="font-bold">Zip Code</label>
-                <input
-                  type="text"
-                  className="bg-white border border-obsidian rounded-md p-2"
-                ></input>
-              </div>
-            </div>
-          </div>
-
-          <div className="flex flex-col space-y-8">
-            <h3 className="text-5xl font-fanwoodText">Payment Information</h3>
-            <div className="flex flex-col space-y-6">
-              <div className="flex flex-col space-y-4 md:flex-row md:space-x-4 md:space-y-0">
-                <div className="flex flex-col space-y-2 md:w-1/2">
-                  <label className="font-bold">Cardholder First Name</label>
-                  <input
-                    type="text"
-                    className="bg-white border border-obsidian rounded-md p-2"
-                  ></input>
+            <div className="flex flex-col space-y-20">
+              <div className="flex flex-col space-y-10 w-fit">
+                <h2 className="font-fanwoodText text-5xl">Donation</h2>
+                <div className="flex flex-row space-x-4">
+                  {donationAmount.map((number, index) => (
+                    <p
+                      key={index}
+                      className={`rounded-full border border-obsidian w-fit py-2 px-6 ${
+                        amount === number && !otherOpen
+                          ? "bg-obsidian text-chalk"
+                          : "hover:bg-rose-100 hover:text-obsidian"
+                      }`}
+                      onClick={() => {setAmount(number); setOtherOpen(false)}}
+                    >
+                      ${number}
+                    </p>
+                  ))}
+                  <p
+                    className={`rounded-full border border-obsidian w-fit py-2 px-6 ${
+                      otherOpen
+                        ? "bg-obsidian text-chalk"
+                        : "hover:bg-rose-100 hover:text-obsidian"
+                    }`}
+                    onClick={() => setOtherOpen(!otherOpen)}
+                  >
+                    Other
+                  </p>
                 </div>
-                <div className="flex flex-col space-y-2 md:w-1/2">
-                  <label className="font-bold">Cardholder Last Name</label>
-                  <input
-                    type="text"
-                    className="bg-white border border-obsidian rounded-md p-2"
-                  ></input>
+
+                <div className={`${otherOpen ? "flex flex-col space-y-2 w-1/2" : "hidden"}`}>
+                  <label className="font-bold">Amount</label>
+                  <div className="flex flex-row border border-obsidian rounded-md justify-between bg-obsidian">
+                    <p className=" text-chalk p-2 w-1/6 text-center">
+                      $
+                    </p>
+                    <input
+                      type="text"
+                      value={amount}
+                      onChange={(e) => setAmount(e.target.value)}
+                      className="bg-white text-end w-full px-4 py-2"
+                    ></input>
+                    <p className="b text-chalk p-2 w-1/6 text-center">
+                      USD
+                    </p>
+                  </div>
+                </div>
+                <div className="flex flex-col space-y-2">
+                  <label className="font-bold">Donation Note</label>
+                  <textarea
+                    className="bg-white border border-obsidian rounded-md p-4"
+                    rows="4"
+                  ></textarea>
                 </div>
               </div>
-
-              <div className="flex flex-col space-y-2">
-                <label className="font-bold">Card Number</label>
-                <input
-                  type="text"
-                  className="bg-white border border-obsidian rounded-md p-2"
-                ></input>
-              </div>
-
-              <div className="flex flex-col space-y-4 md:flex-row md:space-x-4 md:space-y-0">
-                <div className="flex flex-col">
-                  <label className="font-bold">Month</label>
-                  <input
-                    type="text"
-                    className="bg-white border border-obsidian rounded-md p-2"
-                  ></input>
-                </div>
-                <div className="flex flex-col">
-                  <label className="font-bold">Year</label>
-                  <input
-                    type="text"
-                    className="bg-white border border-obsidian rounded-md p-2"
-                  ></input>
-                </div>
-              </div>
-
-              <div className="flex flex-col space-y-">
-                <label className="font-bold">CVC</label>
-                <input
-                  type="text"
-                  className="bg-white border border-obsidian rounded-md p-2"
-                ></input>
-              </div>
             </div>
-          </div>
-        </div>
 
-        <button
-          type="button"
-          className="bg-obsidian text-chalk rounded-md p-4 font-bold text-xl hover:bg-cinnabar transition-all duration-500 ease-in-out"
-        >
-          Donate ${amount} now
-        </button>
-      </form>
-
+            <button
+              type="button"
+              className="bg-obsidian text-chalk rounded-md p-4 font-bold text-xl hover:bg-cinnabar transition-all duration-500 ease-in-out"
+            >
+              Donate ${amount} now
+            </button>
+          </form>
         </div>
       </div>
 
